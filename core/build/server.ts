67056a240f291459fee293cc03ffbfad84776dc3
Djancyp <you@example.com>
2020-07-24 22:38:36 +0200

    Manifest.json and image changes

--- core/build/server.ts
+++ core/build/server.ts
@@ -57,7 +57,7 @@ const serve = (path, cache) => express.static(resolve(path), {
 })
 
 app.use(compression({ threshold: 0 }))
-app.use(favicon('./public/logo-48.png'))
+app.use(favicon('./public/icon-72x72.png'))
 app.use('/dist', serve('../../dist', true))
 app.use('/public', serve('../../public', true))
 app.use('/assets', serve('../../src/themes/default/assets', true))
